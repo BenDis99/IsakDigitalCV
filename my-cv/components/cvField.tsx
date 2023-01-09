ea5ddb65951d2c9e1fdc417dfe1087f809a6a077
Isak Hølleland <isak.holleland@knowit.no>
2023-01-09 18:30:30 +0100

    Fixed errors an added styling to cvField

--- my-cv/components/cvField.tsx
+++ my-cv/components/cvField.tsx
@@ -1,12 +1,24 @@
-import { stringify } from "querystring";
+import styles from "../styles/cvField.module.css"
 
+type jobType = {jobTitle: string, company: string, timefrom: Date, timeto: Date, jobDescription: string, key:string}
 const CvField = (props:any) => {
-    const {job} = props;
+    const {jobs} = props;
+    let allJobs = jobs as jobType[]
+    let uniqueNr = 123;
+    for(let i = 0; i < allJobs.length; i++){
+        allJobs[i].key = `${123 + i}`;
+    }
     
-    return (<div>
-        <h1>{job.jobTitle}, {job.company} | {!!job.timefrom ? job.timefrom : "now"} - {!!job.timeto ? job.timeto : "now"}</h1>
-        <h4>{job.jobDescription}</h4>
-        </div>)
+    return (
+        <div className={styles.cv_field}>
+            {allJobs.map(job => (
+                <div key={job.key} className={styles.cv_field_job}>
+                    <p>{`${job.jobTitle}, ${job.company} | ${!!job.timefrom ? job.timefrom : "now"} - ${!!job.timeto ? job.timeto : "now"}`}</p>
+                    <p>{job.jobDescription}</p>
+                </div>
+            ))}
+        </div>
+        )
 }
 
 export default CvField;
