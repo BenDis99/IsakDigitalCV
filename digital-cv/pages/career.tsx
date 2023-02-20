9ef564352cf34f195bb5da01c1c53a3cdb126d6a
Isak Hølleland <isak_holleland@hotmail.com>
2023-02-20 12:10:15 +0100

    Populated careerPage with my job resume

--- digital-cv/pages/career.tsx
+++ digital-cv/pages/career.tsx
@@ -1,9 +1,29 @@
-const Career = () => {
+import client from "../client";
+import CvField from "../components/cvField";
+import { JobInformation } from "../model/jobInformation";
+import { WelcomeInformation } from "../model/welcomeInformation";
+import styles from '../styles/Home.module.css'
 
+const Career = (props:{jobs : JobInformation[]}) => {
+    const {jobs} = props;
     return (
-        <div>
-            This is career
-        </div>
+      <div>
+        <h1 className={styles.title}>
+            My jobs
+        </h1>
+        <CvField jobs={jobs}/>
+      </div>
     )
+  }
+
+export async function getServerSideProps() {
+    const jobs = await client.fetch(
+        `*[_type == "jobInformation"]`
+    ) as JobInformation[]
+    return {
+        props: {
+            jobs
+        },
+    }
 }
 export default Career;
--- digital-cv/pages/index.tsx
+++ digital-cv/pages/index.tsx
@@ -1,5 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import client from '../client'
 import styles from '../styles/Home.module.css'
 import CvField from "../components/cvField";
@@ -8,28 +6,23 @@ import { JobInformation } from '../model/jobInformation';
 
 
 
-export default function Home(props:{welcomeInformation : WelcomeInformation, jobs : JobInformation[]}) {
-  const {welcomeInformation, jobs} = props;
+export default function Home(props:{welcomeInformation : WelcomeInformation}) {
+  const {welcomeInformation} = props;
   return (
     <div>
       <h1 className={styles.title}>
         {welcomeInformation.title}
       </h1>
-      <CvField jobs={jobs}/>
     </div>
   )
 }
 export async function getServerSideProps() {
   const welcomeInformation = await client.fetch(
     `*[_type == "welcomeInformation"][0]`
-  ) as WelcomeInformation
-  const jobs = await client.fetch(
-    `*[_type == "jobInformation"]`
-  ) as JobInformation[]
+  ) as WelcomeInformation;
   return {
     props: {
-      welcomeInformation,
-      jobs
+      welcomeInformation
     },
   }
 }
