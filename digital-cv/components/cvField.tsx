c46e7b464200273f78bd8edfae9ad09e479e5e71
Isak Hølleland <isak_holleland@hotmail.com>
2023-02-20 15:06:46 +0100

    Styled jobslist

--- digital-cv/components/cvField.tsx
+++ digital-cv/components/cvField.tsx
@@ -4,8 +4,8 @@ import styles from "../styles/cvField.module.css"
 const CvField = (props:{jobs:JobInformation[]}) => {
     const {jobs} = props;
     return (
-        <div className={styles.cv_field}>
-            <dl>
+        <div className={styles.jobs_content}>
+            <dl className={styles.jobs_list}>
             {jobs.reverse().map(job => (<>
                 <dt>{`${job.jobTitle}, ${job.company} | ${!!job.timefrom ? job.timefrom : "now"} - ${!!job.timeto ? job.timeto : "now"}`}</dt>
                 <dd className={styles.descriptionText}>{job.jobDescription}</dd>
