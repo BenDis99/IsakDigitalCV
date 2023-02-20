05a359228daa4d57ce19a5e2aecc63c0937c8210
Isak Hølleland <isak_holleland@hotmail.com>
2023-02-20 13:44:04 +0100

    Styled header

--- digital-cv/components/Header.tsx
+++ digital-cv/components/Header.tsx
@@ -1,14 +1,19 @@
+import Link from "next/link";
 import styles from "../styles/Header.module.css"
+import logo from "../public/favicon.ico"
+import Image from "next/image";
 
 const Header = () => {
 
     return (
         <header className={styles.header}>
+            <Link className={styles.logo_link} href="/">
+                <Image className={styles.logo_image} src={logo} alt="logo"></Image>
+            </Link>
             <nav className={styles.nav}>
-                <a href="/">Logo</a>
-                <a href="/education">Education</a>
-                <a href="/career">Career</a>
-                <a href="/about-me">About me</a>
+                <Link className={styles.subpage_link} href="/utdanning">Utdanning</Link>
+                <Link className={styles.subpage_link} href="/karriere">Karriere</Link>
+                <Link className={styles.subpage_link} href="/om-meg">Om me</Link>
             </nav>
         </header>
     )
