8b5b9a7e0c65079ff2b0a9e7b5f4db339a959c8b
Isak Hølleland <isak_holleland@hotmail.com>
2023-03-07 10:58:03 +0100

    Started on about-me-page

--- digital-cv/pages/om-meg.tsx
+++ digital-cv/pages/om-meg.tsx
@@ -1,8 +1,19 @@
+import styles from "../styles/About.module.css";
+import profilePic from "../public/assets/profile.jpg"
+import Image from "next/image";
+
 const About = () => {
 
     return (
-        <div>
-            This is about-me
+        <div className={styles.page}>
+            <div className={styles.aboutInfo}>
+                <h1 className={styles.h1 +" "+ styles.font_BebasNeue}>Isak Hølleland</h1>
+                <h2 className={styles.h2 +" "+ styles.font_BebasNeue}>Fullstack Developer</h2>
+                <p>The best developer of all</p>
+                <button>Hire me</button>
+            </div>
+            
+            <Image className={styles.profileImage} src={profilePic} alt="picture of a lovely pal with glasses and a beard"/>
         </div>
     )
 }
